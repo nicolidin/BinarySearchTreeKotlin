2af3696d4f5e20694c40c4c274036d0d6a6691b7
Lidin <nicolas.lidin@gmail.com>
2020-10-28 00:47:16 +0100

    refactor -> clean and add comments

--- src/main/kotlin/MyBinary.kt
+++ src/main/kotlin/MyBinary.kt
@@ -1,13 +1,13 @@
 import java.lang.RuntimeException
 
-data class Contact(val name: String, val phoneNumber: Int , val address: String)
+data class Contact(val name: String, val phoneNumber: String , val address: String)
 
 data class MyNode(val value: Contact, var leftChild: MyNode? = null, var rightChild: MyNode? = null)
 
 class MyBinary {
-    var rootNode: MyNode? = null;
+    private var rootNode: MyNode? = null;
 
-    private fun insertNode(currentNode: MyNode, newNode: MyNode) {
+    private tailrec fun insertNode(currentNode: MyNode, newNode: MyNode) {
         if (newNode.value.phoneNumber < currentNode.value.phoneNumber) {
             if (currentNode.leftChild == null) {
                 currentNode.leftChild = newNode;
@@ -35,18 +35,14 @@ class MyBinary {
         }
     }
 
-    private fun findMinNode(currentNode: MyNode): MyNode {
+    private tailrec fun findMinNode(currentNode: MyNode): MyNode {
         if (currentNode.leftChild == null) {
             return currentNode;
         }
-
-        println("ap")
-
         return this.findMinNode(currentNode.leftChild!!);
-
     }
 
-    private fun findParentNodeByChildKey(currentNode: MyNode, key: Int) : MyNode? {
+    private tailrec fun findParentNodeByChildKey(currentNode: MyNode, key: String) : MyNode? {
         if (currentNode.leftChild?.value?.phoneNumber == key || currentNode.rightChild?.value?.phoneNumber == key) {
             return currentNode
         }
@@ -63,36 +59,7 @@ class MyBinary {
         }
     }
 
-    fun deleteNode(key: Int) {
-        val nodeToDelete = this.findNodeByKey(this.rootNode!!, key) ?: throw RuntimeException("can't delete non existing node");
-        val parentOfNodeToDelete = this.findParentNodeByChildKey(this.rootNode!!, key);
-
-        val nodeSuccessor : MyNode? = when {
-            nodeToDelete.rightChild != null -> {
-                this.findMinNode(nodeToDelete.rightChild!!)
-            }
-            nodeToDelete.leftChild != null -> {
-                nodeToDelete.leftChild
-            }
-            else -> null // leaf node
-        }
-        val parentOfSuccessor = if (nodeSuccessor != null && nodeToDelete !== this.rootNode)  this.findParentNodeByChildKey(this.rootNode!!, nodeSuccessor!!.value.phoneNumber) else null;
-        // Remove successor from parent node of successor children
-        if (parentOfSuccessor?.leftChild != null) {
-           parentOfSuccessor.leftChild = null
-        }
-        // assign node successor to parent child of node to delete
-        if (nodeToDelete === parentOfNodeToDelete?.leftChild) {
-           parentOfNodeToDelete.leftChild = nodeSuccessor;
-        } else if (nodeToDelete === parentOfNodeToDelete?.rightChild) {
-            parentOfNodeToDelete.rightChild = nodeSuccessor;
-        }
-        nodeSuccessor?.leftChild = nodeToDelete.leftChild;
-        if (nodeSuccessor != nodeToDelete.rightChild) { nodeSuccessor?.rightChild = nodeToDelete.rightChild } // assign nodeSuccessor.rightChild only if findMinNode got a left leaf node.
-        if (rootNode === nodeToDelete) { this.rootNode = nodeSuccessor }
-    }
-
-    private fun findNodeByKey(currentNode: MyNode, key: Int): MyNode? {
+    private tailrec fun findNodeByKey(currentNode: MyNode, key: String): MyNode? {
         return when {
             key < currentNode.value.phoneNumber -> {
                 currentNode.leftChild ?: return null
@@ -105,7 +72,44 @@ class MyBinary {
             else -> currentNode
         }
     }
-    fun searchNode(key: Int): MyNode? {
+
+    fun deleteNode(key: String) {
+        val nodeToDelete = this.findNodeByKey(this.rootNode!!, key) ?: throw RuntimeException("can't delete non existing node");
+        val parentOfNodeToDelete = this.findParentNodeByChildKey(this.rootNode!!, key);
+
+        val nodeSuccessor : MyNode? = when {
+            nodeToDelete.rightChild != null -> {
+                this.findMinNode(nodeToDelete.rightChild!!)
+            }
+            nodeToDelete.leftChild != null -> {
+                nodeToDelete.leftChild
+            }
+            else -> null // leaf node
+        }
+        when {
+            // assign node successor or null to child of parent node to delete
+            nodeToDelete === parentOfNodeToDelete?.leftChild -> {
+                parentOfNodeToDelete.leftChild = nodeSuccessor;
+            }
+            nodeToDelete === parentOfNodeToDelete?.rightChild -> {
+                parentOfNodeToDelete.rightChild = nodeSuccessor;
+            }
+            nodeToDelete === this.rootNode -> { this.rootNode = nodeSuccessor }
+        }
+        val parentOfSuccessor = if (nodeSuccessor != null && nodeToDelete !== this.rootNode) {
+            this.findParentNodeByChildKey(this.rootNode!!, nodeSuccessor.value.phoneNumber)
+        } else null;
+        // remove successor from it's parent (parentOfSuccessor)
+        if (parentOfSuccessor?.leftChild != null) {
+           parentOfSuccessor.leftChild = null
+        }
+        //assign delete side on nodeSuccessor
+        nodeSuccessor?.leftChild = nodeToDelete.leftChild;
+        if (nodeSuccessor != nodeToDelete.rightChild) { nodeSuccessor?.rightChild = nodeToDelete.rightChild } // assign nodeSuccessor.rightChild only if findMinNode got a left leaf node else if it's the same.
+    }
+
+
+    fun searchNode(key: String): MyNode? {
         this.rootNode ?: throw RuntimeException("can't find node for an empty tree");
 
         return this.findNodeByKey(this.rootNode!!, key)
@@ -114,19 +118,22 @@ class MyBinary {
 
 fun main() {
     val myBinary = MyBinary()
-    myBinary.addNode(Contact("nico", 8, "24 allée gabriel peri"))
-    myBinary.addNode(Contact("nico", 3, "24 allée gabriel peri"))
-    myBinary.addNode(Contact("nico", 10, "24 allée gabriel peri"))
-    myBinary.addNode(Contact("nico", 1, "24 allée gabriel peri"))
-    myBinary.addNode(Contact("nico", 6, "24 allée gabriel peri"))
-    myBinary.addNode(Contact("nico", 14, "24 allée gabriel peri"))
-    myBinary.addNode(Contact("nico", 4, "24 allée gabriel peri"))
-    myBinary.addNode(Contact("nico", 7, "24 allée gabriel peri"))
-    myBinary.addNode(Contact("nico", 13, "24 allée gabriel peri"))
 
-    println(myBinary.searchNode(8))
-    myBinary.deleteNode(8)
-    println(myBinary.searchNode(10))
+    myBinary.addNode(Contact("nico", "8", "24 allée gabriel peri"))
+    myBinary.addNode(Contact("nico", "3", "24 allée gabriel peri"))
+    myBinary.addNode(Contact("nico", "9", "24 allée gabriel peri"))
+    myBinary.addNode(Contact("nico", "1", "24 allée gabriel peri"))
+    myBinary.addNode(Contact("nico", "6", "24 allée gabriel peri"))
+    myBinary.addNode(Contact("nico", "14", "24 allée gabriel peri"))
+    myBinary.addNode(Contact("nico", "4", "24 allée gabriel peri"))
+    myBinary.addNode(Contact("nico", "7", "24 allée gabriel peri"))
+    myBinary.addNode(Contact("nico", "13", "24 allée gabriel peri"))
+
+    println("yow first print")
+    println(myBinary.searchNode("8"))
+    myBinary.deleteNode("8")
+    println("yow scond print")
+    println(myBinary.searchNode("9"))
 }
 
 
