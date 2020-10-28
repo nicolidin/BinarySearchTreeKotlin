858296350952884b79a442121e7425e0a3c40959
Lidin <nicolas.lidin@gmail.com>
2020-10-28 15:50:42 +0100

    refactor -> change value by pair wirth K and T generics

--- src/main/kotlin/MyBinarySearchTree.kt
+++ src/main/kotlin/MyBinarySearchTree.kt
@@ -2,19 +2,19 @@ import java.lang.RuntimeException
 
 data class Contact(val name: String, val phoneNumber: String , val address: String)
 
-data class Node(val value: Contact, var leftChild: Node? = null, var rightChild: Node? = null)
+data class Node<K: Comparable<K>, V>(val content: Pair<K, V>, var leftChild: Node<K, V>? = null, var rightChild: Node<K, V>? = null)
 
-class MyBinarySearchTree {
-    private var rootNode: Node? = null;
+class MyBinarySearchTree<K : Comparable<K>, V> {
+    var rootNode: Node<K, V>? = null;
 
-    private tailrec fun insertNode(currentNode: Node, newNode: Node) {
-        if (newNode.value.phoneNumber < currentNode.value.phoneNumber) {
+    private tailrec fun insertNode(currentNode: Node<K, V>, newNode: Node<K, V>) {
+        if (newNode.content.first < currentNode.content.first) {
             if (currentNode.leftChild == null) {
                 currentNode.leftChild = newNode;
             } else {
                 insertNode(currentNode.leftChild!!, newNode)
             }
-        } else if (newNode.value.phoneNumber > currentNode.value.phoneNumber) {
+        } else if (newNode.content.first > currentNode.content.first) {
             if (currentNode.rightChild == null) {
                 currentNode.rightChild = newNode;
             } else {
@@ -25,23 +25,23 @@ class MyBinarySearchTree {
         }
     }
 
-    private tailrec fun findMinNode(currentNode: Node): Node {
+    private tailrec fun findMinNode(currentNode: Node<K, V>): Node<K, V> {
         if (currentNode.leftChild == null) {
             return currentNode;
         }
         return this.findMinNode(currentNode.leftChild!!);
     }
 
-    private tailrec fun findParentNodeByChildKey(currentNode: Node, key: String) : Node? {
-        if (currentNode.leftChild?.value?.phoneNumber == key || currentNode.rightChild?.value?.phoneNumber == key) {
+    private tailrec fun findParentNodeByChildKey(currentNode: Node<K, V>, key: K) : Node<K, V>? {
+        if (currentNode.leftChild?.content?.first == key || currentNode.rightChild?.content?.first == key) {
             return currentNode
         }
         return when {
-            key < currentNode.value.phoneNumber -> {
+            key < currentNode.content.first -> {
                 currentNode.leftChild ?: throw RuntimeException("no parent found")
                 findParentNodeByChildKey(currentNode.leftChild!!, key)
             }
-            key > currentNode.value.phoneNumber -> {
+            key > currentNode.content.first -> {
                 currentNode.rightChild ?: throw RuntimeException("no parent found")
                 findParentNodeByChildKey(currentNode.rightChild!!, key)
             }
@@ -49,13 +49,13 @@ class MyBinarySearchTree {
         }
     }
 
-    private tailrec fun findNodeByKey(currentNode: Node, key: String): Node? {
+    private tailrec fun findNodeByKey(currentNode: Node<K, V>, key: K): Node<K, V>? {
         return when {
-            key < currentNode.value.phoneNumber -> {
+            key < currentNode.content.first -> {
                 currentNode.leftChild ?: return null
                 findNodeByKey(currentNode.leftChild!!, key)
             }
-            key > currentNode.value.phoneNumber -> {
+            key > currentNode.content.first -> {
                 currentNode.rightChild ?: return null
                 findNodeByKey(currentNode.rightChild!!, key)
             }
@@ -63,11 +63,11 @@ class MyBinarySearchTree {
         }
     }
 
-    fun deleteNode(key: String) {
+    fun deleteNode(key: K) {
         val nodeToDelete = this.findNodeByKey(this.rootNode!!, key) ?: throw RuntimeException("can't delete non existing node");
         val parentOfNodeToDelete = this.findParentNodeByChildKey(this.rootNode!!, key);
 
-        val nodeSuccessor : Node? = when {
+        val nodeSuccessor : Node<K, V>? = when {
             nodeToDelete.rightChild != null -> {
                 this.findMinNode(nodeToDelete.rightChild!!)
             }
@@ -87,7 +87,7 @@ class MyBinarySearchTree {
             nodeToDelete === this.rootNode -> { this.rootNode = nodeSuccessor }
         }
         val parentOfSuccessor = if (nodeSuccessor != null && nodeToDelete !== this.rootNode) {
-            this.findParentNodeByChildKey(this.rootNode!!, nodeSuccessor.value.phoneNumber)
+            this.findParentNodeByChildKey(this.rootNode!!, nodeSuccessor.content.first)
         } else null;
         // remove successor from it's parent (parentOfSuccessor)
         if (parentOfSuccessor?.leftChild != null) {
@@ -99,14 +99,14 @@ class MyBinarySearchTree {
     }
 
 
-    fun searchNode(key: String): Node? {
+    fun searchNode(key: K): Node<K, V>? {
         this.rootNode ?: throw RuntimeException("can't find node for an empty tree");
 
         return this.findNodeByKey(this.rootNode!!, key)
     }
 
-    fun addNode(value: Contact) {
-        val newNode =  Node(value)
+    fun addNode(content: Pair<K, V>) {
+        val newNode =  Node(content)
 
         if (rootNode == null) {
             this.rootNode = newNode;
@@ -117,23 +117,38 @@ class MyBinarySearchTree {
 }
 
 fun main() {
-    val myBinary = MyBinarySearchTree()
+    val myBinaryWithKeyPhone = MyBinarySearchTree<String, Contact>()
 
-    myBinary.addNode(Contact("nico", "8", "24 allée gabriel peri"))
-    myBinary.addNode(Contact("nico", "3", "24 allée gabriel peri"))
-    myBinary.addNode(Contact("nico", "9", "24 allée gabriel peri"))
-    myBinary.addNode(Contact("nico", "1", "24 allée gabriel peri"))
-    myBinary.addNode(Contact("nico", "6", "24 allée gabriel peri"))
-    myBinary.addNode(Contact("nico", "14", "24 allée gabriel peri"))
-    myBinary.addNode(Contact("nico", "4", "24 allée gabriel peri"))
-    myBinary.addNode(Contact("nico", "7", "24 allée gabriel peri"))
-    myBinary.addNode(Contact("nico", "13", "24 allée gabriel peri"))
+    myBinaryWithKeyPhone.addNode(Pair("123", Contact("nico", "+353 1 666 9354", "24 allée gabriel peri")))
+    myBinaryWithKeyPhone.addNode(Pair("003", Contact("jean", "+353 1 256 9720", "11 allée gabriel peri")))
+    myBinaryWithKeyPhone.addNode(Pair("246", Contact("thibault", "+353 1 256 9876", "12 allée gabriel peri")))
+    myBinaryWithKeyPhone.addNode(Pair("001", Contact("adrien", "+353 1 256 2968", "25 allée gabriel peri")))
+    myBinaryWithKeyPhone.addNode(Pair("113", Contact("laurianne", "+353 1 121 2968", "122 allée gabriel peri")))
+    myBinaryWithKeyPhone.addNode(Pair("234", Contact("clément", "+353 1 139 9876", "176 allée gabriel peri")))
+    myBinaryWithKeyPhone.addNode(Pair("256", Contact("stéphane", "+353 1 258 9799", "211 allée gabriel peri")))
+    myBinaryWithKeyPhone.addNode(Pair("258", Contact("hugo", "+353 1 258 0099", "1 allée gabriel peri")))
 
-    println("yow first print")
-    println(myBinary.searchNode("8"))
-    myBinary.deleteNode("8")
-    println("yow scond print")
-    println(myBinary.searchNode("9"))
+    println(myBinaryWithKeyPhone.searchNode("123"))
+    myBinaryWithKeyPhone.deleteNode("123")
+    println(myBinaryWithKeyPhone.rootNode)
+
+    //println(myBinaryWithKeyPhone.searchNode("234"))
+
+   // val myBinaryWithKeyName = MyBinarySearchTree<String, Contact>()
+
+   // myBinaryWithKeyName.addNode(Pair("nico", Contact("nico", "+353 1 666 9354", "24 allée gabriel peri")))
+   // myBinaryWithKeyName.addNode(Pair("jean", Contact("jean", "+353 1 256 9720", "11 allée gabriel peri")))
+   // myBinaryWithKeyName.addNode(Pair("thibault", Contact("thibault", "+353 1 256 9876", "12 allée gabriel peri")))
+   // myBinaryWithKeyName.addNode(Pair("adrien", Contact("adrien", "+353 1 256 2968", "25 allée gabriel peri")))
+   // myBinaryWithKeyName.addNode(Pair("laurianne", Contact("laurianne", "+353 1 121 2968", "122 allée gabriel peri")))
+   // myBinaryWithKeyName.addNode(Pair("clément", Contact("clément", "+353 1 139 9876", "176 allée gabriel peri")))
+   // myBinaryWithKeyName.addNode(Pair("stéphane", Contact("stéphane", "+353 1 258 9799", "211 allée gabriel peri")))
+   // myBinaryWithKeyName.addNode(Pair("hugo", Contact("hugo", "+353 1 258 0099", "1 allée gabriel peri")))
+   // myBinaryWithKeyName.addNode(Pair("paulo", Contact("paulo", "+353 1 111 0099", "2 allée gabriel peri")))
+
+   // println(myBinaryWithKeyName.searchNode("nico"))
+   // myBinaryWithKeyName.deleteNode("nico")
+   // println(myBinaryWithKeyName.searchNode("jean"))
 }
 
 
